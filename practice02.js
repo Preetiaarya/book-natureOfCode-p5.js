01e9d5c7d9be84474fd0189102b1d2bdec9ca603
Preeti Aarya <kumaripreetiaarya00@gmail.com>
2024-12-06 14:05:59 +0530

    add rect speed on upwards

--- practice02.js
+++ practice02.js
@@ -1,12 +1,33 @@
-let randomLight = [];
+// let randomLight = [];
 
-let light = 20;
-function setup(){
-    createCanvas(1000,600);
-    for(let i = 0; i<light; i++){
-    randomLight[i]=0;
-    }
+// let light = 20;
+// function setup(){
+//     createCanvas(1000,600);
+//     for(let i = 0; i<light; i++){
+//     randomLight[i]=0;
+//     }
+// }
+// function draw(){
+//     background("yellow");
+// }
+
+let rectY = 600; // Initial Y position of the rectangle
+let speed = 5; // Speed of the rectangle
+
+function setup() {
+  createCanvas(1000, 600); // Create a canvas of 400x400
 }
-function draw(){
-    background("yellow");
+
+function draw() {
+  background(220); // Clear the background
+  
+  fill(100, 200, 255); // Set rectangle color
+  rect(450, rectY, 20, 50); // Draw the rectangle (x, y, width, height)
+
+  rectY -= speed; // Move the rectangle upwards
+
+  // If the rectangle moves beyond the canvas height, reset to the top
+  if (rectY < 0) {
+    rectY = height;
+  }
 }
