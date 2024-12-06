43a1a85d3525196bbf24d54cfff5d244efac8222
Preeti Aarya <kumaripreetiaarya00@gmail.com>
2024-12-06 13:43:18 +0530

    added practice02 file in book

---
+++ practice02.js
@@ -0,0 +1,12 @@
+let randomLight = [];
+
+let light = 20;
+function setup(){
+    createCanvas(1000,600);
+    for(let i = 0; i<light; i++){
+    randomLight[i]=0;
+    }
+}
+function draw(){
+    background("yellow");
+}
